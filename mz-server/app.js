33dbed488f2f6eda1067a77908773c12e895d8dd
huangsisheng <1173091704@qq.com>
2019-10-14 17:08:17 +0800

    登录zhuce

--- mz-server/app.js
+++ mz-server/app.js
@@ -2,16 +2,47 @@ let Koa = require('koa')
 let app = new Koa()
 const fs = require('fs')
 const static = require('koa-static')   //静态资源服务插件
-var Router = require('koa-router');
-var router = new Router();
+const Router = require('koa-router');
+
+const router = new Router();
 const path = require('path')
+// post提交数据源
+const bodyParser = require('koa-bodyparser');
+app.use(bodyParser());
+
 const staticPath = './src/assets'
 // 配置静态web服务的中间件
 app.use(static(
     path.join(__dirname, staticPath)
 ))
 
+//引入数据库操作方法
+const UserController = require('./src/controller/user');
+//checkToken作为中间件存在
+const checkToken = require('./src/token/checkToken');
+
+//登录
+const loginRouter = new Router();
+loginRouter.post('/login', UserController.Login);
+//注册
+const registerRouter = new Router();
+registerRouter.post('/register', UserController.Reg);
+
+//获取所有用户
+const userRouter = new Router();
+userRouter.get('/user', checkToken, UserController.GetAllUsers);
+//删除某个用户
+const delUserRouter = new Router();
+delUserRouter.post('/delUser', checkToken, UserController.DelUser);
+
+//装载上面四个子路由
+router.use('/api', loginRouter.routes(), loginRouter.allowedMethods());
+router.use('/api', registerRouter.routes(), registerRouter.allowedMethods());
+router.use('/api', userRouter.routes(), userRouter.allowedMethods());
+router.use('/api', delUserRouter.routes(), delUserRouter.allowedMethods());
+
 router.get('/gateway', async (ctx) => {
+    
     ctx.response.type = 'json'
     ctx.response.body = fs.createReadStream('./src/assets/json/getcity.json')
 })
